cf6851e561975657e48d3f1a84ebf023c7593308
masozee <nurojilukmansyah@gmail.com>
2021-06-19 09:25:18 +0700

    update template

--- web/url.py
+++ web/url.py
@@ -3,5 +3,9 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='dashboard/index.html'), name='home'),
-    path('hatespeech', TemplateView.as_view(template_name='hatespeech/index.html'), name='hatespeech'),
+    path('hatespeech/', TemplateView.as_view(template_name='hatespeech/index.html'), name='hatespeech'),
+    path('hatespeech/syiah/', TemplateView.as_view(template_name='hatespeech/hatespeech.html'), name='syiah'),
+    path('hatespeech/cina/', TemplateView.as_view(template_name='hatespeech/hatespeech.html'), name='cina'),
+    path('hatespeech/ahmadiyah/', TemplateView.as_view(template_name='hatespeech/hatespeech.html'), name='ahmadiyah'),
+    path('hatespeech/hoax/', TemplateView.as_view(template_name='hatespeech/hatespeech.html'), name='hoax'),
 ]
