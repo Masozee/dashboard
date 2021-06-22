f5978fda5da96c8ba8efa9055664eeab38cd35f7
masozee <nurojilukmansyah@gmail.com>
2021-06-22 11:43:36 +0700

    perbaikan homepage

--- web/url.py
+++ web/url.py
@@ -2,10 +2,10 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='dashboard/index.html'), name='home'),
+    path('', TemplateView.as_view(template_name='dashboard/index2.html'), name='home'),
     path('hatespeech/', TemplateView.as_view(template_name='hatespeech/index.html'), name='hatespeech'),
     path('hatespeech/syiah/', TemplateView.as_view(template_name='hatespeech/hatespeech.html'), name='syiah'),
     path('hatespeech/cina/', TemplateView.as_view(template_name='hatespeech/hatespeech.html'), name='cina'),
     path('hatespeech/ahmadiyah/', TemplateView.as_view(template_name='hatespeech/hatespeech.html'), name='ahmadiyah'),
-    path('hatespeech/hoax/', TemplateView.as_view(template_name='hatespeech/hatespeech.html'), name='hoax'),
+    path('hatespeech/hoax/', TemplateView.as_view(template_name='hatespeech/grid.html'), name='hoax'),
 ]
